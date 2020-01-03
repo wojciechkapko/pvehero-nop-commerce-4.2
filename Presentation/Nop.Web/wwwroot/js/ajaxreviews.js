81a4709abcbd164bd43e7c555930ab078ce65970
wojciech-kapko <wojciech.kapko@gmail.com>
2020-01-03 19:52:49 +0100

    add product review page style and scripts

---
+++ Presentation/Nop.Web/wwwroot/js/ajaxreviews.js
@@ -0,0 +1,14 @@
+﻿var AjaxReviews = (function () {
+
+    var initRating = function () {
+        $('#rating.hover .star.rate').click(function () {
+            var rate = $(this).data('rating') - 1;
+            $(this).parent().attr('data-stars', $(this).data('rating'));
+            $("#addproductrating_" + rate).prop("checked", true);
+        });
+    };
+
+    return {
+        initRating: initRating
+    };
+})();
