5652877ccdf0754b607a32ef19e89e462b1f3438
wojciech-kapko <wojciech.kapko@gmail.com>
2020-01-04 23:30:05 +0100

    add chevron down icon

--- Presentation/Nop.Web/wwwroot/js/loadicons.js
+++ Presentation/Nop.Web/wwwroot/js/loadicons.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var file = '/sprites/regular.svg',
-        revision = 97;
+        revision = 98;
 
     if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)
         return true;
