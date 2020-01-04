0e59a46c57135bb7236f2764e012d02561425fd7
wojciech-kapko <wojciech.kapko@gmail.com>
2020-01-04 02:31:45 +0100

    change paypal icon name

--- Presentation/Nop.Web/wwwroot/js/loadicons.js
+++ Presentation/Nop.Web/wwwroot/js/loadicons.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var file = '/sprites/regular.svg',
-        revision = 96;
+        revision = 97;
 
     if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)
         return true;
