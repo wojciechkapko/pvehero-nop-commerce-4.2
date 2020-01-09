ddc444d19dc8592c84dd2379129baac3776e56bc
wojciech-kapko <wojciech.kapko@gmail.com>
2020-01-09 18:18:03 +0100

    add new icons for catalog view

--- Presentation/Nop.Web/wwwroot/js/loadicons.js
+++ Presentation/Nop.Web/wwwroot/js/loadicons.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var file = '/sprites/regular.svg',
-        revision = 101;
+        revision = 103;
 
     if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)
         return true;
