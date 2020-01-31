ee852e738e74ab61354b6c1538f48b41cda391f7
wojciech-kapko <wojciech.kapko@gmail.com>
2020-01-31 02:52:39 +0100

    - style for image selection boxes on product details page - icon cleanup and fixes

--- Presentation/Nop.Web/wwwroot/js/loadicons.js
+++ Presentation/Nop.Web/wwwroot/js/loadicons.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var file = '/sprites/regular.svg',
-        revision = 114;
+        revision = 115;
 
     if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)
         return true;
